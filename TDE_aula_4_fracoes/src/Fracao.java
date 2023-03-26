85fc4ab3a6c3b9ab07a86456c42b9992fdfdccff
Guilherme Borba <guivborba@hotmail.com>
2023-03-26 17:58:12 -0300

    Add files via upload

---
+++ TDE_aula_4_fracoes/src/Fracao.java
@@ -0,0 +1,13 @@
+class Fracao {
+    int numerador;
+    int denominador;
+    Fracao(int num, int den) {
+        numerador = num;
+        denominador = den;
+    }
+    Fracao multiplicar(Fracao outra) {
+        int novoNumerador = numerador * outra.numerador;
+        int novoDenominador = denominador * outra.denominador;
+        return new Fracao(novoNumerador, novoDenominador);
+    }
+}
---
+++ TDE_aula_4_fracoes/src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Fracao f1 = new Fracao(2, 3);
+        Fracao f2 = new Fracao(3, 4);
+
+        Fracao resultado = f1.multiplicar(f2);
+
+        System.out.println("Resultado: " + resultado.numerador + "/" + resultado.denominador);
+    }
+}
